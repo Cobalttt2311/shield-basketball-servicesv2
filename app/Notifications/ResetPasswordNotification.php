d1b9b20feda5c489fb93641eaea122fb0e17b8c2
Akmal <akmalnur140@gmail.com>
2026-07-17 11:36:02 +0700

    change url forgot password

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -24,12 +24,12 @@ class ResetPasswordNotification extends Notification
 
     public function toMail($notifiable)
     {
-        $resetUrl = config('app.frontend_url', 'http://localhost:3000').'/reset-password?'.http_build_query([
+        $resetUrl = config('app.frontend_url', 'http://52.64.55.223').'/reset-password?'.http_build_query([
             'token' => $this->token,
             'email' => $notifiable->email,
         ]);
 
-        $logoUrl = config('app.frontend_url', 'http://localhost:3000').'/img/logo/logo-light-full.png';
+        $logoUrl = config('app.frontend_url', 'http://52.64.55.223').'/img/logo/logo-light-full.png';
 
         return (new MailMessage)
             ->subject('Reset Password')
